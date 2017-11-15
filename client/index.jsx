26662895c08bf22f9b8e6f27911d871ea9ef18d2
RudyRed <willgross94@gmail.com>
2017-11-15 14:49:25 -0800

    Initializing react-router

--- client/index.jsx
+++ client/index.jsx
@@ -1,11 +1,12 @@
-// import React from 'react';
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+// import App from './components/App';
 
-// import ReactDOM from 'react-dom';
-
-// class App extends React.component {
-//   constructor(props) {
-//     super(props);
-//   }
-// }
-
-// ReactDOM.render(<App />, document.getElementById('app'));
+render(
+  (
+    <BrowserRouter>
+      {/* <App /> */}
+    </BrowserRouter>
+  ), document.getElementById('root'),
+);
