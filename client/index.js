e392342983dabc28ef17a4f07ccde7b116fcb2bb
Brian Pak <brianpak27@gmail.com>
2017-11-14 16:02:00 -0800

    stubbing app

---
+++ client/index.js
@@ -0,0 +1,10 @@
+import React from ('react');
+import ReactDOM from ('ReactDom');
+
+class App extends React.component{
+  constructor(props) {
+    super(props);
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
