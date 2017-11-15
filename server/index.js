135fa5a7ade98f9ee67b7a1eaefef1ab6087b04c
Carrie Zhang <carrie@Carries-MacBook-Pro.local>
2017-11-15 11:58:57 -0800

    generated webpack.json.js and bundle.js, and created index.html

--- server/index.js
+++ server/index.js
@@ -1,3 +1,8 @@
-// const express = require('express');
+const express = require('express');
+const app = express();
 
-// const app = express();
+app.use(express.static(__dirname + './../client/dist'));
+
+app.listen(8080, function () {
+  console.log('listening on port 8080')
+});
