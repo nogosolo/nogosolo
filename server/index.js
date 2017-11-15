9d723a073178333d5b70ba82d6e5eccf39e56721
Carrie Zhang <carrie@Carries-MacBook-Pro.local>
2017-11-15 12:00:30 -0800

    fixed syntax error

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
+
 const express = require('express');
+
 const app = express();
 
-app.use(express.static(__dirname + './../client/dist'));
+app.use(express.static(`${__dirname}/../client/dist`));
 
-app.listen(8080, function () {
-  console.log('listening on port 8080')
+app.listen(8080, () => {
+  console.log('listening on port 8080');
 });
