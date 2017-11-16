273fbad712732ce4fb4f3d89c5ae9bae7f4e66f0
Carrie Zhang <carrie@Carries-MacBook-Pro.local>
2017-11-15 19:48:54 -0800

    imported body-parser, and deleted db folder

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(express.static(`${__dirname}/../client/dist`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/', (req, res) => {
+
+});
+
+app.get('/', (req, res) => {
+
+});
 
 app.listen(8080, () => {
   console.log('listening on port 8080');
