6054c86ddd7981c08a3a9783b471045048ab4dba
Carrie Zhang <carrie@carries-mbp.lan>
2017-11-20 15:55:12 -0800

    created MatchPage, and installed react-image-slider, but can not peer with react@16.1.1 TBD

--- server/index.js
+++ server/index.js
@@ -9,16 +9,35 @@ app.use(express.static(`${__dirname}/../client/dist`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
+app.post('/dummyData', (req, res) => {
+  // console.log(typeof db.query);
+  dummyData.dummyData.forEach((entry) => {
+    db.query(`INSERT INTO users (name, username, password, bio, picture)
+    VALUES ('${entry.name}', '${entry.username}', '${entry.password}', '${entry.bio}', '${entry.picture}')`)
+      .then(() => {
+        console.log('successfully added dummy data');
+        res.end();
+      })
+      .catch((err) => {
+        console.log('THIS IS AN ERROR', err);
+      });
+  });
+});
+
+
 // `INSERT INTO users (username, password, )
 //   VALUES ("${entry.username}", )`
 
-app.post('/', (req, res) => {
+// app.post('/', (req, res) => {
 
-});
+// app.post('/', (req, res) => {
 
-app.get('/', (req, res) => {
+// });
 
-});
+// app.get('/', (req, res) => {
+
+// });
 
 function initialDBPopulation() {
   dummyData.dummyData.forEach((entry) => {
