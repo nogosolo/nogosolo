abb89e2efd5b0de5a7b43e7809f1233accc284ab
RudyRed <willgross94@gmail.com>
2017-11-22 12:16:18 -0800

    Setup server to populate empty DB on start with dummy data

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const dummyData = require('../database/dummyData.js');
@@ -10,21 +9,6 @@ app.use(express.static(`${__dirname}/../client/dist`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/dummyData', (req, res) => {
-  // console.log(typeof db.query);
-  dummyData.dummyData.forEach((entry) => {
-    db.query(`INSERT INTO users (name, username, password, bio, picture)
-    VALUES ('${entry.name}', '${entry.username}', '${entry.password}', '${entry.bio}', '${entry.picture}')`)
-      .then(() => {
-        console.log('successfully added dummy data');
-        res.end();
-      })
-      .catch((err) => {
-        console.log('THIS IS AN ERROR', err);
-      });
-  });
-});
-
 // `INSERT INTO users (username, password, )
 //   VALUES ("${entry.username}", )`
 
@@ -35,6 +19,26 @@ app.post('/', (req, res) => {
 app.get('/', (req, res) => {
 
 });
+
+function initialDBPopulation() {
+  dummyData.dummyData.forEach((entry) => {
+    db.query(`INSERT INTO users (name, username, password, bio, picture)
+    VALUES ('${entry.name}', '${entry.username}', '${entry.password}', '${entry.bio}', '${entry.picture}')`)
+      .then(() => {
+        console.log(`${entry.name} with username ${entry.username} was successfully added to the DB`);
+      })
+      .catch((err) => {
+        console.log('THIS IS AN ERROR', err);
+      });
+  });
+}
+
+db.query('select * from users')
+  .then((data) => {
+    if (!data.length) {
+      initialDBPopulation();
+    }
+  });
 
 app.listen(8080, () => {
   console.log('listening on port 8080');
