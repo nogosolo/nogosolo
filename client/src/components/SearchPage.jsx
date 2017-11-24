8984c5558565972954aa0d0fae8044c6c3331782
RudyRed <willgross94@gmail.com>
2017-11-24 11:29:39 -0800

    Added venue and data details to search page

--- client/src/components/SearchPage.jsx
+++ client/src/components/SearchPage.jsx
@@ -18,7 +18,7 @@ class Search extends React.Component {
       datatype: 'json',
       url: `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apiKey}&keyword=${this.props.searchQuery}`,
       success: (data) => {
-        const eventNames = data._embedded.events.map(event => event.name);
+        const eventNames = data._embedded.events;
         window.x = data; // just to look at stuff in chrome
         this.setState({
           results: eventNames,
@@ -39,7 +39,14 @@ class Search extends React.Component {
           <div>
             <h3>Search Results for: {this.state.keyword} </h3>
             There Are {this.state.results.length} event results.
-            {this.state.results.map((eventNames) => { return <li>{eventNames}</li>; })}
+            {this.state.results.map((event) => {
+              return <li>
+                {event.name} {' '}
+                {event.dates.start.localDate} {' '}
+                {event._embedded.venues["0"].address.line1} {' '}
+                {event._embedded.venues["0"].city.name} {' '}
+                {event._embedded.venues["0"].country.countryCode} {' '}
+              </li>; })}
           </div>
       );
     }
