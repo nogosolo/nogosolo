bd427c2ee909d27cc68db0a5ac290ca0ffe96d95
RudyRed <willgross94@gmail.com>
2017-11-23 17:48:43 -0800

    created a super hacky way to initially populate the match table RM when test data no longer required!!!!

--- server/index.js
+++ server/index.js
@@ -49,6 +49,64 @@ app.post('/signup', (req, res) => {
 });
 
 
+function addPotentialMatch(userid, eventid) {
+  db.query(`SELECT * FROM user_event
+    WHERE eventId = '${eventid}' AND NOT userId = ${userid}`)
+    .then((data) => {
+      data.forEach((entry) => {
+        db.query(`SELECT * FROM match
+          WHERE user1 = ${userid} AND user2 = ${entry.userid}`)
+          .then((matchEntry) => {
+            if (!matchEntry.length) {
+              db.query(`INSERT INTO match (user1, user2)
+              VALUES (${userid}, ${entry.userid})`)
+                .then(() => {
+                  console.log(`potential match with ${userid}, ${entry.userid} added`);
+                });
+              db.query(`INSERT INTO match (user1, user2)
+                VALUES (${entry.userid}, ${userid})`) // use this commented out part in reality
+                .then(() => {
+                  console.log(`potential match with ${entry.userid}, ${userid} added`);
+                });
+            }
+          })
+          .catch((err) => {
+            console.log('THIS IS AN ERROR', err);
+          });
+      });
+    })
+    .catch((err) => {
+      console.log('THIS IS AN ERROR', err);
+    });
+}
+
+
+function addPotentialMatchInit(userid, eventid) { // temporary to populate database
+  db.query(`SELECT * FROM user_event
+    WHERE eventId = '${eventid}' AND NOT userId = ${userid}`)
+    .then((data) => {
+      data.forEach((entry) => {
+        db.query(`SELECT * FROM match
+          WHERE user1 = ${userid} AND user2 = ${entry.userid}`)
+          .then((matchEntry) => {
+            if (!matchEntry.length) {
+              db.query(`INSERT INTO match (user1, user2)
+              VALUES (${userid}, ${entry.userid})`)
+                .then(() => {
+                  console.log(`potential match with ${userid}, ${entry.userid} added`);
+                });
+            }
+          })
+          .catch((err) => {
+            console.log('THIS IS AN ERROR', err);
+          });
+      });
+    })
+    .catch((err) => {
+      console.log('THIS IS AN ERROR', err);
+    });
+}
+
 function initialDBPopulation() {
   dummyData.forEach((entry) => {
     db.query(`INSERT INTO users (name, username, password, bio, picture)
@@ -57,8 +115,9 @@ function initialDBPopulation() {
         db.query(`SELECT id FROM users WHERE username = '${entry.username}'`)
           .then((data) => {
             entry.events.forEach((event) => {
+              const eventStr = `${event}d`;
               const queryStr = `INSERT INTO user_event (userid, eventid)
-              VALUES (${data[0].id}, ${event})`;
+              VALUES (${data[0].id}, '${eventStr}')`;
               db.query(queryStr)
                 .then(() => {
                   console.log(`${entry.name} going to event: ${event} added to DB`);
@@ -72,6 +131,17 @@ function initialDBPopulation() {
       });
   });
 }
+
+db.query('select * from user_event')// temporary to populate database
+  .then((data) => {
+    if (data.length >= 124) {
+      data.forEach((userevent) => {
+        console.log('userevent.userid', userevent.eventid);
+        addPotentialMatchInit(userevent.userid, userevent.eventid);
+      });
+    }
+  });
+// select distinct user2 from match where user1 = 1
 
 db.query('select * from users')
   .then((data) => {
