5bc2a3ee99b571ed28e4e3f3b0c9a7a1d13ab8e2
RudyRed <willgross94@gmail.com>
2017-11-26 14:35:44 -0800

    added a view your matches page and styling for it

--- client/src/components/SiteNavBar.jsx
+++ client/src/components/SiteNavBar.jsx
@@ -3,32 +3,17 @@ import { Link } from 'react-router-dom';
 import LoginButton from './LoginButton';
 import SearchBox from './SearchBox';
 
-// The Header creates links that can be used to navigate
-// between routes.
-// const SiteNavBar = function (props) {
-//   return (
-//     <header className="header">
-//       <nav className="bar">
-//           <span className="home"><Link to="/" >Home</Link></span>
-//           <SearchBox className="searchbar" searchHandler={props.searchHandler} />
-//           {!props.isLoggedin && (<span className="signupbutton" type="submit"><Link to="/signup">Sign up</Link></span>)}
-//           {!props.isLoggedin && (<span className="loginbutton"><LoginButton triggerLogin={props.triggerLogin} /></span>)}
-//           <Link to="/match"> Find Your Match </Link>
-//       </nav>
-//     </header>
-//   );
-// };
-
-const SiteNavBar = function(props) {
+const SiteNavBar = function (props) {
   return (
     <div className="topnav" id="myTopnav">
       <Link to="/" >Home</Link>
       {!props.isLoggedin && (<Link to="/signup">Sign up</Link>)}
       {!props.isLoggedin && (<LoginButton triggerLogin={props.triggerLogin} />)}
-      <Link to="/match">Match </Link>
+      <Link to="/match">Find a Match </Link>
+      <Link to="/viewMatches">View Your Matches </Link>
       <SearchBox searchHandler={props.searchHandler} />
     </div>
-  )
-}
+  );
+};
 
 export default SiteNavBar;
