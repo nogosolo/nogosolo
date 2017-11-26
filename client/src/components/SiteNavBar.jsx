7c9d40bdbd07077cafca0e25c99fa2b390c8813f
Carrie Zhang <carrie@Carries-MacBook-Pro.local>
2017-11-25 17:02:40 -0800

    updated css.style also refactor the coresponding components

--- client/src/components/SiteNavBar.jsx
+++ client/src/components/SiteNavBar.jsx
@@ -5,20 +5,30 @@ import SearchBox from './SearchBox';
 
 // The Header creates links that can be used to navigate
 // between routes.
-const SiteNavBar = function (props) {
+// const SiteNavBar = function (props) {
+//   return (
+//     <header className="header">
+//       <nav className="bar">
+//           <span className="home"><Link to="/" >Home</Link></span>
+//           <SearchBox className="searchbar" searchHandler={props.searchHandler} />
+//           {!props.isLoggedin && (<span className="signupbutton" type="submit"><Link to="/signup">Sign up</Link></span>)}
+//           {!props.isLoggedin && (<span className="loginbutton"><LoginButton triggerLogin={props.triggerLogin} /></span>)}
+//           <Link to="/match"> Find Your Match </Link>
+//       </nav>
+//     </header>
+//   );
+// };
+
+const SiteNavBar = function(props) {
   return (
-    <header>
-      <nav className="bar">
-        <span>
-          <span className="home"><Link to="/" >Home</Link></span>
-          {!props.isLoggedin && (<span className="signupbutton"><Link to="/signup">Sign up</Link></span>)}
-          {!props.isLoggedin && (<span className="loginbutton"><LoginButton triggerLogin={props.triggerLogin} /></span>)}
-          <Link to="/match"> Find Your Match </Link>
-          <b><SearchBox className="searchbar" searchHandler={props.searchHandler} /></b>
-        </span>
-      </nav>
-    </header>
-  );
-};
+    <div className="topnav" id="myTopnav">
+      <a href="/">Home</a>
+      {!props.isLoggedin && (<Link to="/signup">Sign up</Link>)}
+      {!props.isLoggedin && (<LoginButton triggerLogin={props.triggerLogin} />)}
+      <Link to="/match">Match </Link>
+      <SearchBox className="searchbar" searchHandler={props.searchHandler} />
+    </div>
+  )
+}
 
 export default SiteNavBar;
